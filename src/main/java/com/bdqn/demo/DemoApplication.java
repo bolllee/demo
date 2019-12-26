73b97ba107078f9d7f18fcbdc089a6797d5a5031
Boll <1464766428@qq.com>
2019-12-26 21:37:53 +0800

    ti

--- src/main/java/com/bdqn/demo/DemoApplication.java
+++ src/main/java/com/bdqn/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.bdqn.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/demo/xrf/Test.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.xrf;
-
-public class Test {
-}
